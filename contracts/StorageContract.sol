32be33cc67ec5bacad8b79822e6f2511646e8cb5
alvin-reyes <areyes@LAPTOP-978P3C3I.localdomain>
2021-07-31 21:33:15 -0400

    new: added function to store directory hash

--- contracts/StorageContract.sol
+++ contracts/StorageContract.sol
@@ -13,8 +13,18 @@ contract StorageContract {
     string fileHash;
   }
 
+  struct Directory {
+    uint directoryID;
+    address directoryOwner;
+    string directorName;
+    string directoryHash;
+  }
+
   uint public fileID = 0;
   mapping (uint => File) public fileStorage;
+
+  uint public directoryID = 0;
+  mapping (uint => Directory) public directoryStorage;
 
   //  Add file function
   function addFile(string memory _fileName, string memory _fileHash, address owner) public returns (bool status) {
@@ -22,6 +32,15 @@ contract StorageContract {
     address _fileOwner = owner;
     File memory newFile = File(fileID, _fileOwner, _fileName, _fileHash);
     fileStorage[fileID] = newFile;
+    
+    return true;
+  }
+
+  function uploadDirectory(string memory _directorName, string memory _directoryHash, address owner) public returns (bool status) {
+    directoryID += 1;
+    address _directoryOwner = owner;
+    Directory memory newFile = Directory(directoryID, _directoryOwner, _directorName, _directoryHash);
+    directoryStorage[directoryID] = newFile;
     
     return true;
   }
